6c361b2b2562d64668d98d36078d54aec84df43f
Flame25 <christopherrichard2525@gmail.com>
2025-05-25 04:15:44 +0700

    Update for takeoff2 (works with takeoff mode)

--- build/keyboard/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/gadzz/vtol_ws/install/keyboard_msgs/share/keyboard_msgs/package.sh"
--- build/mission_center/CMakeFiles/pilot_node.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for pilot_node.
-# This may be replaced when dependencies are built.
